c4a2bf28ed516ed0139e3ce703d456041aa5024d
Agraj Yadav <agrajy10@gmail.com>
2022-02-24 20:39:53 +0530

    store current color theme in localStorage

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,9 +1,20 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    if (localStorage.getItem('theme')) {
+      setTheme(localStorage.getItem('theme'));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return <AppContext.Provider value={{ theme, setTheme }}>{children}</AppContext.Provider>;
 };
 
